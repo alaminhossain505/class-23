f7ad9a4b96d3bc9b96f96a7c20af725b7835671d
unknown <alaminhossainovi505@gmail.com>
2021-05-20 14:42:36 +0600

    first comment

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+const text = document.getElementsByClassName('text-area')[0];
+const count = document.getElementsByClassName('count-area')[0];
+
+
+function allLetter ()
+{
+        const countAll = text.value.length;
+        count.innerHTML = `${countAll}`;
+}
+
+// 1st Selection
+
+const text1 = document.getElementsByClassName('text-area1')[0];
+const count1 = document.getElementsByClassName('count-area1')[0];
+
+
+function AllLetter ()
+{
+        const countAll1 = text1.value.length;
+        count1.innerHTML = `${countAll1}`;
+}
+
+// 2nd Selection
+const text2 = document.getElementsByClassName('text-area2')[0];
+const count2 = document.getElementsByClassName('count-area2')[0];
+
+
+function AllLetter1 ()
+{
+        const countAll2 = text2.value.length;
+        count2.innerHTML = `${countAll2}`;
+}
+
+// 3rd Selection
+
